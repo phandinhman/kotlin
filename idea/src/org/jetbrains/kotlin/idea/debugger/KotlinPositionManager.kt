e9b717036f0595bda6c8b9ba01fa05c8f700f18c
Nikolay Krasko <Nikolay.Krasko@jetbrains.com>
2016-11-16 18:31:11 +0300

    -- Can't invoke getVirtualMachineProxy() not from debugger manager thread (used in isDexDebug)

--- idea/src/org/jetbrains/kotlin/idea/debugger/KotlinPositionManager.kt
+++ idea/src/org/jetbrains/kotlin/idea/debugger/KotlinPositionManager.kt
@@ -42,6 +42,7 @@ import org.jetbrains.kotlin.idea.debugger.breakpoints.getLambdasAtLineIfAny
 import org.jetbrains.kotlin.idea.debugger.evaluate.KotlinCodeFragmentFactory
 import org.jetbrains.kotlin.idea.debugger.evaluate.KotlinDebuggerCaches
 import org.jetbrains.kotlin.idea.decompiler.classFile.KtClsFile
+import org.jetbrains.kotlin.idea.refactoring.getLineCount
 import org.jetbrains.kotlin.idea.refactoring.getLineStartOffset
 import org.jetbrains.kotlin.idea.stubindex.KotlinSourceFilterScope
 import org.jetbrains.kotlin.idea.util.ProjectRootsUtil
@@ -120,8 +121,12 @@ class KotlinPositionManager(private val myDebugProcess: DebugProcess) : MultiReq
             return SourcePosition.createFromElement(elementInDeclaration)
         }
 
-        val (line, ktFile) = ktLocationInfo(location, myDebugProcess.isDexDebug(), myDebugProcess.project, false, psiFile)
-        return SourcePosition.createFromLine(ktFile ?: psiFile, line - 1)
+        if (sourceLineNumber > psiFile.getLineCount() && myDebugProcess.isDexDebug()) {
+            val (line, ktFile) = ktLocationInfo(location, true, myDebugProcess.project, false, psiFile)
+            return SourcePosition.createFromLine(ktFile ?: psiFile, line - 1)
+        }
+
+        return SourcePosition.createFromLine(psiFile, sourceLineNumber)
     }
 
     // Returns a property or a constructor if debugger stops at class declaration
